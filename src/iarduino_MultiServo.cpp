fcd57256d5755be97930a6ae08bb7863d19286a8
TremaUser1 <39560516+TremaUser1@users.noreply.github.com>
2024-05-16 19:56:53 +0300

    1.1.4
    avr,esp8266,esp32,mbed_rp2040,renesas_uno

--- src/iarduino_MultiServo.cpp
+++ src/iarduino_MultiServo.cpp
@@ -2,7 +2,7 @@
 
 /**	основные пользовательские функции **/
 //		инициализация чипа
-void	iarduino_MultiServo::begin(uint8_t i, uint16_t j){
+void	iarduino_MultiServo::PCA_begin(uint8_t i, uint16_t j){
 		PCA_uint_ID=i; PCA_uint_PWM=j; PCA_func_SET_MODE(); digitalWrite(SERVO_ALL,LOW);					//	сохраняем адрес чипа и частоту ШИМ, устанавливаем регистры режимов работы, устанавливаем все выходы в логические «0»
 }
 //		установка ШИМ на выходе
@@ -12,19 +12,19 @@ void	iarduino_MultiServo::analogWrite(uint8_t i, uint16_t j, uint16_t k){
 		if(j>4095){digitalWrite(i,HIGH); return;}															//	Устанавливаем логическую 1 
 		if(k>4095){k=4095;}	j+=k; if(j>4095){j-=4096;}														//	Отодвигаем спад импульса в зависимости от фазового сдвига
 		uint8_t data[4]={k,k>>8,j,j>>8};																	//	Определяем массив data из 4 байт данных для записи
-		objI2C->writeBytes(PCA_uint_ID, (i==SERVO_ALL?0xFA:(i*4+6)), data, 4);								//	Записываем 4 байта массива data в модуль PCA_uint_ID начиная с регистра (i==SERVO_ALL?0xFA:(i*4+6))
+		selI2C->writeBytes(PCA_uint_ID, (i==SERVO_ALL?0xFA:(i*4+6)), data, 4);								//	Записываем 4 байта массива data в модуль PCA_uint_ID начиная с регистра (i==SERVO_ALL?0xFA:(i*4+6))
 }
 //		установка логического уровня на выходе
 void	iarduino_MultiServo::digitalWrite(uint8_t i, bool j){
 		if(i>=16&&i!=SERVO_ALL){return;}																	//	игнорируем команду
 		uint8_t data[4]={0,j?16:0,0,j?0:16};																//	Определяем массив data из 4 байт данных для записи
-		objI2C->writeBytes(PCA_uint_ID, (i==SERVO_ALL?0xFA:(i*4+6)), data, 4);								//	Записываем 4 байта массива data в модуль PCA_uint_ID начиная с регистра (i==SERVO_ALL?0xFA:(i*4+6))
+		selI2C->writeBytes(PCA_uint_ID, (i==SERVO_ALL?0xFA:(i*4+6)), data, 4);								//	Записываем 4 байта массива data в модуль PCA_uint_ID начиная с регистра (i==SERVO_ALL?0xFA:(i*4+6))
 }
 //		чтение установленного коэффициента заполнения на выходе
 uint16_t iarduino_MultiServo::analogRead(uint8_t i){
 		if(i>=16){return 0;} uint16_t j=0, k=0;																//	игнорируем команду
 		uint8_t data[4];																					//	Объявляем массив data из 4 элементов
-		objI2C->readBytes(PCA_uint_ID, (i*4+6), data, 4);													//	Заполняем массив data читая 4 байта из модуля PCA_uint_ID начиная с регистра (i*4+6)
+		selI2C->readBytes(PCA_uint_ID, (i*4+6), data, 4);													//	Заполняем массив data читая 4 байта из модуля PCA_uint_ID начиная с регистра (i*4+6)
 		if(data[3]&0x10){return 0;}																			//	на читаемом выходе установлен 0
 		if(data[1]&0x10){return 4096;}																		//	на читаемом выходе установлена 1
 		k=(data[1]<<8)+data[0];																				//	количество тактов до фронта импульса
@@ -53,25 +53,22 @@ void	iarduino_MultiServo::servoSet(uint8_t i, uint8_t j){
 
 /**	дополнительные пользовательские функции **/
 //		инверсия сигналов на всех выходах
-void	iarduino_MultiServo::invert(bool i){PCA_flag_INVRT=i; PCA_func_SET_MODE();}										//	установка или сброс флага INVRT
+void	iarduino_MultiServo::invert(bool i){PCA_flag_INVRT=i; PCA_func_SET_MODE();}							//	установка или сброс флага INVRT
 //		выбор интегрированной схемы подключения выходов в чипе
-void	iarduino_MultiServo::outdrv(bool i){PCA_flag_OUTDRV=i; PCA_func_SET_MODE();}									//	установка или сброс флага OUTDRV
+void	iarduino_MultiServo::outdrv(bool i){PCA_flag_OUTDRV=i; PCA_func_SET_MODE();}						//	установка или сброс флага OUTDRV
 //		состояние на выходах при подаче на вход OE уровня логической «1»
-void	iarduino_MultiServo::outState(uint8_t i){if(i>2){return;} PCA_uint_OUTNE=i; PCA_func_SET_MODE();}				//	установка двух битов OUTNE
+void	iarduino_MultiServo::outState(uint8_t i){if(i>2){return;} PCA_uint_OUTNE=i; PCA_func_SET_MODE();}	//	установка двух битов OUTNE
 //		установка частоты внешнего источника тактирования
 void	iarduino_MultiServo::extClock(uint16_t i){if(i){PCA_flag_EXTCLK=true; PCA_uint_OSC=i; PCA_uint_OSC*=1000; PCA_func_SET_MODE();}else{PCA_flag_EXTCLK=false; restart(); PCA_uint_OSC=25000000; PCA_func_SET_MODE(); digitalWrite(SERVO_ALL,LOW);}}
 //		перезагрузка всех чипов на шине
-void	iarduino_MultiServo::restart(void){objI2C->writeByte(0x0, 0x6);}													//	отправка команды «SWRST Call»
-//		установка скорости шины
-void	iarduino_MultiServo::bus(uint16_t i){objI2C->begin(i); PCA_flag_I2C=true;}										//	инициализация шины I2C с установкой частоты (в кГц)
+void	iarduino_MultiServo::restart(void){uint8_t i; selI2C->writeBytes(0x0, 0x6, &i, 0);}					//	отправка команды «SWRST Call». Отправляем в модуль с адресом 0, номер регистра 6, не передавая данные в этот регистр.
 //		чтение/запись одного байта данных из/в регистр
-uint8_t	iarduino_MultiServo::reg(uint8_t i           ){return objI2C->readByte (PCA_uint_ID, i   );}
-uint8_t	iarduino_MultiServo::reg(uint8_t i, uint8_t j){return objI2C->writeByte(PCA_uint_ID, i, j);}
+uint8_t	iarduino_MultiServo::reg(uint8_t i           ){return selI2C->readByte (PCA_uint_ID, i   );}
+uint8_t	iarduino_MultiServo::reg(uint8_t i, uint8_t j){return selI2C->writeByte(PCA_uint_ID, i, j);}
 
 /**	внутренние функции **/
 //		установка регистров режимов работы MODE1, MODE2 и PRE_SCALE
 void	iarduino_MultiServo::PCA_func_SET_MODE(){
-		if(!PCA_flag_I2C){PCA_flag_I2C=true; objI2C->begin(100);}											//	инициализация шины I2C с установкой частоты в 100 кГц (если она не инициирована ранее)
 		uint16_t j=(PCA_uint_OSC/(4096*uint32_t(PCA_uint_PWM)))-1; if(j<3){j=3;} if(j>255){j=255;}			//	определяем значение предделителя
 		reg(0,0x30);																						//	отправляем байт данных в регистр MODE1     (устанавливаем флаги AL и SLEEP, остальные флаги сброшены)
 		reg(0xFE,j);																						//	отправляем байт данных в регистр PRE_SCALE (устанавливаем предделитель для частоты ШИМ)
--- src/iarduino_MultiServo.h
+++ src/iarduino_MultiServo.h
@@ -1,5 +1,5 @@
 //	Библиотека для работы с Multi Servo Shield: http://iarduino.ru/shop/Expansion-payments/multi-servo-shield-na-16-servoprivodov.html
-//  Версия: 1.1.3
+//  Версия: 1.1.4
 //  Последнюю версию библиотеки Вы можете скачать по ссылке: http://iarduino.ru/file/255.html
 //  Подробное описание функции бибилиотеки доступно по ссылке: http://wiki.iarduino.ru/page/multi-servo-shield-na-16-servoprivodov/
 //  Библиотека является собственностью интернет магазина iarduino.ru и может свободно использоваться и распространяться!
@@ -17,7 +17,14 @@
 #include <WProgram.h>																						//
 #endif																										//
 																											//
-#include <iarduino_MultiServo_I2C.h>																		//	Подключаем файл iarduino_MultiServo_I2C.h - для работы с шиной I2C        (используя функции производного класса iarduino_I2C)
+#include "iarduino_MultiServo_I2C.h"																		//	Подключаем библиотеку выбора реализации шины I2C.
+																											//
+#if defined(TwoWire_h) || defined(__ARDUINO_WIRE_IMPLEMENTATION__) || defined(__AVR_ATmega328__) || defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1284P__) || defined(__AVR_ATmega2560__) || defined(ESP8266) || defined(ESP32) || defined(ARDUINO_ARCH_RP2040) || defined(RENESAS_CORTEX_M4) // Если подключена библиотека Wire или платы её поддерживают...
+#include <Wire.h>																							//	Разрешаем использовать библиотеку Wire в данной библиотеке.
+#endif																										//
+#if defined( iarduino_I2C_Software_h )																		//	Если библиотека iarduino_I2C_Software подключена в скетче...
+#include <iarduino_I2C_Software.h>																			//	Разрешаем использовать библиотеку iarduino_I2C_Software в данной библиотеке.
+#endif																										//
 																											//
 #define	IMPEDANCE			2																				//	третье состояние выхода (состояние высокого импеданса - состояние высокого сопротивления - состояние отключённого выхода)
 #define	SERVO_ALL			255																				//	№ выхода используемый как указание значений всем выходам
@@ -29,9 +36,14 @@
 class iarduino_MultiServo{																					//
 	public:																									//
 	/**	Конструктор класса **/																				//
-		iarduino_MultiServo				(){	objI2C = new iarduino_I2C; }									//	Конструктор основного класса
+		iarduino_MultiServo				(){	selI2C = new iarduino_I2C_Select; }								//	Конструктор основного класса
 	/**	основные пользовательские функции **/																//
-		void	begin					(uint8_t=0x40, uint16_t=50);										//	инициализ-я	чипа										(ID адрес, частота ШИМ в Гц)
+		#if defined(TwoWire_h) || defined(__ARDUINO_WIRE_IMPLEMENTATION__)									//
+		void		begin				(TwoWire* i=&Wire, uint8_t id=0x40, uint16_t pwm=50){ selI2C->begin(i); PCA_begin(id,pwm); }	//	Определяем функцию инициализации модуля	(Параметр:  объект для работы с аппаратной шиной I2C, ID адрес, частота ШИМ в Гц).
+		#endif																								//
+		#if defined(iarduino_I2C_Software_h)																//
+		void		begin				(SoftTwoWire* i  , uint8_t id=0x40, uint16_t pwm=50){ selI2C->begin(i); PCA_begin(id,pwm); }	//	Определяем функцию инициализации модуля	(Параметр:  объект для работы с программной шиной I2C, ID адрес, частота ШИМ в Гц).
+		#endif																								//
 		void	analogWrite				(uint8_t, uint16_t, uint16_t=0);									//	установка	ШИМ на выходе								(№ выхода от 0 до 15, коэффициент заполнения от 0 до 4095, фазовый сдвиг от 0 до 4095)
 		void	digitalWrite			(uint8_t, bool);													//	установка	логического уровня на выходе				(№ выхода от 0 до 15, LOW / HIGH)
 	   uint16_t	analogRead				(uint8_t);															//	чтение		коэффициента заполнения на выходе			(№ выхода от 0 до 15)
@@ -45,26 +57,25 @@ class iarduino_MultiServo{																					//
 		void	outState				(uint8_t=0);														//	состояние	на всех выходах при подаче «1» на вход OE	(LOW / HIGH / IMPEDANCE) примечание: при указании HIGH и выборе схемы с открытым стоком, на выходах будет состояние высокого импеданса
 		void	extClock				(uint16_t=0);														//	установка	частоты внешнего источника тактирования		(частота в кГц, от 1 до 50000)
 		void	restart					();																	//	перезагр-ка	всех чипов на шине							(без параметров)
-		void	bus						(uint16_t=100);														//	установка	скорости работы шины I2C					(частота в кГц)
 		uint8_t	reg						(uint8_t);															//	чтение      регистра									(адрес регистра)
 		uint8_t	reg						(uint8_t, uint8_t);													//	запись      в регистр									(адрес регистра, запысываемый байт)
 																											//
 	private:																								//
 	/**	внутренние переменные **/																			//
-		iarduino_I2C_BASE*				objI2C;																//	Объявляем указатель на объект полиморфного класса iarduino_I2C_BASE, но в конструкторе данного класса этому указателю будет присвоена ссылка на производный класс iarduino_I2C
+		iarduino_I2C_VirtualSelect*		selI2C;																//	Объявляем  указатель  на  объект полиморфного класса iarduino_I2C_VirtualSelect, но в конструкторе текущего класса этому указателю будет присвоена ссылка на производный класс iarduino_I2C_Select.
 		uint8_t	PCA_uint_ID =			0x40;																//	адрес		чипа на шине I2C
 	   uint16_t	PCA_uint_PWM =			50;																	//	частота		ШИМ на выходах чипа в Гц
 	   uint32_t	PCA_uint_OSC =			25000000;															//	частота		генератора тактовых импульсов в Гц, для формирования ШИМ
 	   uint16_t	PCA_uint_ANGLE[16] =	{180,180,180,180,180,180,180,180,180,180,180,180,180,180,180,180};	//	максимальный угол поворота
 	   uint16_t	PCA_uint_ANGLE_MIN[16] ={170,170,170,170,170,170,170,170,170,170,170,170,170,170,170,170};	//	коэффициент заполнения для угла 0°
 	   uint16_t	PCA_uint_ANGLE_MAX[16] ={470,470,470,470,470,470,470,470,470,470,470,470,470,470,470,470};	//	коэффициент заполнения для максимального угла
-		bool	PCA_flag_I2C =			false;																//	флаг		инициализации шины I2C
 		bool	PCA_flag_EXTCLK =		false;																//	флаг		состояния бита EXTCLK
 		bool	PCA_flag_INVRT =		false;																//	флаг		состояния бита INVRT
 		bool	PCA_flag_OUTDRV =		true;																//	флаг		состояния бита OUTDRV
 		uint8_t	PCA_uint_OUTNE =		0;																	//	значение	двух битов OUTNE
 																											//
 	/**	внутренние функции **/																				//
+		void	PCA_begin				(uint8_t, uint16_t);												//	инициализ-я	чипа										(ID адрес, частота ШИМ в Гц)
 		void	PCA_func_SET_MODE();																		//	установка	регистров режимов MODE1, MODE2 и PRE_SCALE	(без параметров)
 };																											//
 																											//
